234a29d81d39c65b2b2aa79ca6666814ba324056
Nick12345-code <astonishedchocolate@gmail.com>
2021-07-22 14:29:08 +1000

    Setup

---
+++ Assets/Scripts/Ragdoll.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+    [SerializeField] private float forceAmount;
+    [SerializeField] private Rigidbody selectedRigidbody;
+    [SerializeField] private float selectionDistance;
+    private Vector3 originalScreenTargetPosition;
+    private Vector3 originalRigidbodyPos;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Check if we are hovering over Rigidbody, if so, select it
+            selectedRigidbody = GetRigidbodyFromMouseClick();
+        }
+
+        if (Input.GetMouseButtonUp(0) && selectedRigidbody)
+        {
+            //Release selected Rigidbody if there any
+            selectedRigidbody = null;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (selectedRigidbody)
+        {
+            Vector3 mousePositionOffset = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, selectionDistance)) - originalScreenTargetPosition;
+            selectedRigidbody.velocity = (originalRigidbodyPos + mousePositionOffset - selectedRigidbody.transform.position) * forceAmount * Time.deltaTime;
+        }
+
+        GetRigidbodyFromMouseClick();
+    }
+
+    Rigidbody GetRigidbodyFromMouseClick()
+    {
+        RaycastHit hitInfo = new RaycastHit();
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        bool hit = Physics.Raycast(ray, out hitInfo);
+        Debug.DrawRay(ray.origin, ray.direction * 50, Color.blue);
+
+        if (hit)
+        {
+            if (hitInfo.collider.gameObject.GetComponent<Rigidbody>())
+            {
+                selectionDistance = Vector3.Distance(ray.origin, hitInfo.point);
+                originalScreenTargetPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, selectionDistance));
+                originalRigidbodyPos = hitInfo.collider.transform.position;
+                return hitInfo.collider.gameObject.GetComponent<Rigidbody>();
+            }
+        }
+
+        return null;
+    }
+}
